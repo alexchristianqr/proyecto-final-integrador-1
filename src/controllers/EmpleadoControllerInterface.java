ff7ba77ae259540ef840d82b85dc8fdea1d6656c
Fernando Sotelo <warsha975@gmail.com>
2024-10-25 11:34:33 -0500

    Revert "controllers"
    This reverts commit 6d5b554bb2eac3bfb04b55fa6c8c29e35f0fcc6e.

--- src/controllers/EmpleadoControllerInterface.java
+++
@@ -1,9 +0,0 @@
-package controllers;
-
-import javax.swing.table.DefaultTableModel;
-import models.Empleado;
-
-public interface EmpleadoControllerInterface {
-    DefaultTableModel listarEmpleados(String buscar);
-    Empleado crearEmpleado(Empleado empleado);
-}
